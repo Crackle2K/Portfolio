ea4466d4137000eabcd9a61c42f0b8a7babcc185
Crackle2K <crackleontwitch@gmail.com>
2025-11-11 16:39:24 -0500

    BOBAAA

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Boba from './components/Boba.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   
   return (
     <>
-      <h1></h1>
+      <h1>Boba Builder</h1>
+      <Boba></Boba>
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/components/Boba.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const cup = null;
+const pearls = null;
+const lid = null;
+const powder = null;
+const straw = null;
+
+const Boba = () => {
+  return (
+    <img></img>
+  )
+}
+
+export default Boba
