407b28a8fcf5987737f163f923606da0c7b909b2
Crackle2K <crackleontwitch@gmail.com>
2025-11-12 09:38:18 -0500

    footer gooning

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Boba from './components/Boba.jsx'
 import Footer from './components/Footer.jsx'
+import ToppingButton from './components/ToppingButton.jsx'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
     <>
       <h1>Boba Builder</h1>
       <Boba></Boba>
+      <ToppingButton></ToppingButton>
       <Footer></Footer>
     </>
   )
---
+++ src/components/ToppingButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ToppingButton = () => {
+  return (
+    <div>
+        <button>Hello</button>
+    </div>
+  )
+}
+
+export default ToppingButton
