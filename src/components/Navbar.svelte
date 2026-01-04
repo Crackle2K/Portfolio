e6d8772bbce547e996f55f51a0e194f26d58dd98
Crackle2K <crackleontwitch@gmail.com>
2026-01-03 20:19:46 -0500

    Reworked in Svelte + Typescript

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { link } from "svelte-spa-router";
+</script>
+
+<nav class="navbar">
+  <div class="nav-content">
+    <a href="/" use:link class="nav-link">Home</a>
+    <a href="/showcase" use:link class="nav-link">Showcase</a>
+    <a href="/achievements" use:link class="nav-link">Certifications</a>
+    <a href="/contact" use:link class="nav-link">Contact</a>
+  </div>
+</nav>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svelte()],
   base: "/",
 })
